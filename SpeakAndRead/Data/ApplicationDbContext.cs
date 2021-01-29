e33154f352c1518b2ad0290cea0ea97516e5f5f4
Eugene1337 <Eugeene1337@users.noreply.github.com>
2021-01-29 11:05:16 +0100

    Init commit

---
+++ SpeakAndRead/Data/ApplicationDbContext.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using SpeakAndRead.Models;
+
+namespace SpeakAndRead.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options): base(options)
+        {
+        }
+        public DbSet<Language> Languages { get; set; }
+        public DbSet<Course> Courses { get; set; }
+        public DbSet<Material> Materials { get; set; }
+        public DbSet<Review> Reviews { get; set; }
+        public DbSet<Lesson> Lessons { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder); // create tables for Identity
+        }
+    }
+}
